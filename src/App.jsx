5f27c50fa1d1daeb3a316aa6516d4489ec39fcab
TWindred <tyler.windred@gmail.com>
2022-01-20 10:53:42 +0000

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import './App.css';
 import Home from './features/home/Home';
 import Header from "./features/header/Header";
-import Subreddits from "./features/subreddits/subreddits";
+import Subreddits from "./features/subreddits/Subreddits";
 
 function App() {
   return (
