d62c9b63596d34e36d224096d7abc33cb8f4c7b3
TWindred <tyler.windred@gmail.com>
2022-01-20 11:31:16 +0000

    changed export statement

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import './card.css';
 
-const Card = (props) => {
+export const Card = (props) => {
     return <div className={`card ${props.className}`}>{props.children}</div>
 };
-
-export default Card;
